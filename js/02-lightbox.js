275005888a8f45873d47929f9189607c4ed38ba8
Locomotive2te116 <locomotive2te116@gmail.com>
2023-11-02 17:29:00 +0200

    add2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+
+const makeGallery = pictureInfo => { 
+    const { preview, original, description } = pictureInfo;
+        return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+            </li>
+         `;
+}
+const arrayGallery = galleryItems.map(pictureInfo => {
+    return makeGallery(pictureInfo);
+});
+
+galleryList.insertAdjacentHTML('beforeend', arrayGallery.join(''));
 
 console.log(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
